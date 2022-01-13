182100d83d0981042f139e147ba3072b5c908f6c
Fahad_Kader <ashikulf@gmail.com>
2022-01-13 00:30:42 -0500

    initial

---
+++ Project1/src/test/java/com/automation/runner/UberBookTest.java
@@ -0,0 +1,20 @@
+package com.automation.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/UberBook.feature"},
+        glue = ("com.automation.stepdef"),
+        tags ="@All",
+
+        plugin = {"pretty"}
+)
+
+
+public class UberBookTest {
+
+}
